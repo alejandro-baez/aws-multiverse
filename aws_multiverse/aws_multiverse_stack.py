31816e9df7f8a674d1bff06b5669e9baea1300ca
Alejandro Baez <abaez2623@gmail.com>
2023-12-13 23:58:15 -0500

    created get route and improved lambda function, working api route

--- aws_multiverse/aws_multiverse_stack.py
+++ aws_multiverse/aws_multiverse_stack.py
@@ -3,11 +3,8 @@ from aws_cdk import (
     Stack,
     aws_lambda,
     aws_apigateway as apigateway, 
-
-    # aws_sqs as sqs,
 )
 from constructs import Construct  
-
 
 
 class AwsMultiverseStack(Stack):
@@ -15,24 +12,20 @@ class AwsMultiverseStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsMultiverseQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+       
         song_info_funtion = aws_lambda.Function(self, id= 'SongNameFunc', code= aws_lambda.Code.from_asset("./compute/"), handler = 'song_info.lambda_handler',runtime = aws_lambda.Runtime.PYTHON_3_8)
 
         api = apigateway.RestApi(self,id="restapi", rest_api_name="get-song-api") 
         
         songs = api.root.add_resource('songs')  
         songs.add_method("GET",apigateway.LambdaIntegration(song_info_funtion))
-        songs 
+        songs.add_method("POST")
 
 
-        # get_song_integration = apigateway.LambdaIntegration(song_info_funtion,
-        #         request_templates={"application/json": '{ "statusCode": "200" }'})
+        song = songs.add_resource("{id}")
+        song.add_method("GET")
+        song.add_method("PUT")
+        song.add_method("DELETE")
+        
 
-        # api.root.add_method("GET", get_song_integration)   # GET /
        
--- compute/song_info.py
+++ compute/song_info.py
@@ -1,18 +1,28 @@
 import json
 
+songs =[]
 def song_info(title:str, artist:str,length:int, album:str):
-    song = {
+    songs.append({
         'title':title,
         'artist': artist,
         'length':length,
         'album':album
-    }
+    })
+    
 
-    return song
+    return songs
 
 def lambda_handler(event,context):
     song = song_info('Yellow','Coldplay',265,'Parachutes')
-    message = f"The songs name is {song['title']}"
+    song_info('High Speed','Coldplay',240,'Parachutes')
+    song_info('Shiver','Coldplay',302,'Parachutes')
+    song_info('Spark','Coldplay',226,'Parachutes')
+    song_info('Trouble','Coldplay',273,'Parachutes')
+    song_info('Parachutes','Coldplay',45,'Parachutes')
+    song_info('We Never Change','Coldplay',248,'Parachutes')
+    song_info('Dont Panic','Coldplay',135,'Parachutes')
 
-    return {'statusCode':200,'body': json.dumps({'message': message, 'song' :song})}
+    message = f"The songs length is {len(song)}"
+
+    return {'statusCode':200,'body': json.dumps({'message': message, 'songs' :song})}
     
